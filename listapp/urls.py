8e6f4566c823e3a8df0779cf402f1d02d1d2afb8
Greg Reeds <gregreeds@hotmail.com>
2024-05-05 12:58:08 +0000

    Add update and delete list views

--- listapp/urls.py
+++ listapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ShoppingLists, ListDetail, ListCreate
+from .views import ShoppingLists, ListDetail, ListCreate, ListUpdate, ListDelete
 
 urlpatterns = [
     path('', ShoppingLists.as_view(), name='lists'),
     path('list/<int:pk>/', ListDetail.as_view(), name='list'),
     path('list-create/', ListCreate.as_view(), name='list-create'),
+    path('list-update/<int:pk>/', ListUpdate.as_view(), name='list-update'),
+    path('list-delete/<int:pk>/', ListDelete.as_view(), name='list-delete'),
 ]
--- listapp/views.py
+++ listapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import List
 
@@ -20,4 +20,15 @@ class ListDetail(DetailView):
 class ListCreate(CreateView):
     model = List
     fields = ['title']
+    success_url = reverse_lazy('lists')
+
+
+class ListUpdate(UpdateView):
+    model = List
+    fields = ['title']
+    success_url = reverse_lazy('lists')
+
+class ListDelete(DeleteView):
+    model = List
+    context_object_name = 'list'
     success_url = reverse_lazy('lists')
