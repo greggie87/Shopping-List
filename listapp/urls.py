6298ca18dfacfce31bfc47c3913a36856d625161
Greg Reeds <gregreeds@hotmail.com>
2024-05-05 11:33:35 +0000

    Add create list view with functionality

--- listapp/urls.py
+++ listapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ShoppingLists, ListDetail
+from .views import ShoppingLists, ListDetail, ListCreate
 
 urlpatterns = [
     path('', ShoppingLists.as_view(), name='lists'),
     path('list/<int:pk>/', ListDetail.as_view(), name='list'),
+    path('list-create/', ListCreate.as_view(), name='list-create'),
 ]
--- listapp/views.py
+++ listapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import List
 
 # Create your views here.
@@ -13,3 +15,9 @@ class ShoppingLists(ListView):
 class ListDetail(DetailView):
     model = List
     context_object_name = 'uniquelist'
+
+
+class ListCreate(CreateView):
+    model = List
+    fields = ['title']
+    success_url = reverse_lazy('lists')
