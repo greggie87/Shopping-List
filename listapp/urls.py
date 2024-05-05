8b0849723c4d6871d4c21932ef9a9510471e769a
Greg Reeds <gregreeds@hotmail.com>
2024-05-05 15:11:22 +0000

    Add register view and functionality

--- listapp/urls.py
+++ listapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ShoppingLists, ListDetail, ListCreate, ListUpdate, ListDelete, CustomLoginView
+from .views import ShoppingLists, ListDetail, ListCreate, ListUpdate, ListDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', ShoppingLists.as_view(), name='lists'),
     path('list/<int:pk>/', ListDetail.as_view(), name='list'),
     path('list-create/', ListCreate.as_view(), name='list-create'),
--- listapp/views.py
+++ listapp/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import List
 
@@ -18,6 +20,24 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('lists')
+
+
+class RegisterPage(FormView):
+    template_name = 'listapp/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('lists')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('lists')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class ShoppingLists(LoginRequiredMixin, ListView):
