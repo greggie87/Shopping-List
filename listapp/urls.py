b762f26bf5dd990b1d9f8787160aa7568440b0f5
Greg Reeds <gregreeds@hotmail.com>
2024-05-05 10:43:34 +0000

    Add template for list detail

--- listapp/urls.py
+++ listapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ShoppingLists
+from .views import ShoppingLists, ListDetail
 
 urlpatterns = [
     path('', ShoppingLists.as_view(), name='lists'),
+    path('list/<int:pk>/', ListDetail.as_view(), name='list'),
 ]
--- listapp/views.py
+++ listapp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import List
 
 # Create your views here.
 
 class ShoppingLists(ListView):
     model = List
+    context_object_name = 'lists'
+
+
+class ListDetail(DetailView):
+    model = List
+    context_object_name = 'uniquelist'
