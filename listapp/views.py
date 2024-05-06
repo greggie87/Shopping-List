b9f9eebced7011048a05754c919c0c0d6ae85e8e
Greg Reeds <gregreeds@hotmail.com>
2024-05-06 03:07:02 +0000

    Style view pages

--- listapp/views.py
+++ listapp/views.py
@@ -52,6 +52,7 @@ class ShoppingLists(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['lists'] = context['lists'].filter(user=self.request.user)
+        context['count'] = context['lists'].filter(complete=False).count()
         return context
 
 
@@ -72,7 +73,7 @@ class ListCreate(LoginRequiredMixin, CreateView):
 
 class ListUpdate(LoginRequiredMixin, UpdateView):
     model = List
-    fields = ['title']
+    fields = ['title', 'complete']
     success_url = reverse_lazy('lists')
 
 
